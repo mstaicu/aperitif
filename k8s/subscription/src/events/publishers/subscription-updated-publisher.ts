e6b3f0f293cfae09db7fed95f8bf1888ff8f6d9d
Mircea Staicu <md.staicu@yahoo.com>
2024-08-11 18:11:22 +0100

    remove subscription service

--- k8s/subscription/src/events/publishers/subscription-updated-publisher.ts
+++
@@ -1,5 +0,0 @@
-import { Publisher, Subjects, SubscriptionUpdatedEvent } from "@tartine/common";
-
-export class SubscriptionUpdatedPublisher extends Publisher<SubscriptionUpdatedEvent> {
-  readonly subject = Subjects.SubscriptionUpdated;
-}
