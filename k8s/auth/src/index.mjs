928b2e7237e218a5ec192187f4132b4648c6e03c
Mircea Staicu <md.staicu@yahoo.com>
2024-10-06 11:46:10 +0100

    minor shuffling around

--- k8s/auth/src/index.mjs
+++ k8s/auth/src/index.mjs
@@ -2,14 +2,14 @@ import nconf from "nconf";
 
 nconf.env();
 
-nconf.required([
-  "AUTH_EXPRESS_PORT",
-  "AUTH_MONGODB_URI",
-  "DOMAIN",
-  "ORIGIN",
-  "REGISTRATION_ACCESS_TOKEN",
-  "REDIS_PORT",
-  "REDIS_HOST",
-]);
+// nconf.required([
+//   "AUTH_EXPRESS_PORT",
+//   "AUTH_MONGODB_URI",
+//   "DOMAIN",
+//   "ORIGIN",
+//   "REGISTRATION_ACCESS_TOKEN",
+//   "REDIS_PORT",
+//   "REDIS_HOST",
+// ]);
 
 import("./server.mjs");
