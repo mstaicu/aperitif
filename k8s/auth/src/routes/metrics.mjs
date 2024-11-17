f5a7c04bb3274db969e076bee5e4599bbe014c9f
Mircea Staicu <md.staicu@yahoo.com>
2024-11-17 19:12:25 +0000

    minor cleanupp

--- k8s/auth/src/routes/metrics.mjs
+++ k8s/auth/src/routes/metrics.mjs
@@ -1,12 +1,12 @@
 // @ts-check
 import { Router } from "express";
-import promClient from "prom-client";
+import { register } from "prom-client";
 
 const router = Router();
 
 router.get("/metrics", async (_, res) => {
-  res.set("Content-Type", promClient.register.contentType);
-  res.end(await promClient.register.metrics());
+  res.set("Content-Type", register.contentType);
+  res.end(await register.metrics());
 });
 
 export { router as metricsRouter };
