9dc6aff69cd3f9ebe4363b3db65b01566b13f805
Mircea Staicu <md.staicu@yahoo.com>
2024-11-25 13:09:43 +0000

    traefik improvements

--- k8s/.tasks/create-k8s-resources.mjs
+++ k8s/.tasks/create-k8s-resources.mjs
@@ -1,10 +1,10 @@
-import { execSync } from "child_process";
-import { generateKeyPairSync } from "node:crypto";
+import { execSync } from "node:child_process";
+import { randomBytes } from "node:crypto";
 
 var namespace = process.env.NAMESPACE;
 
 if (!namespace) {
-  console.error("error: NAMESPACE environment variables must be set.");
+  console.error("error: NAMESPACE environment variable must be set.");
   process.exit(1);
 }
 
@@ -13,6 +13,7 @@ function checkResourceExists(kind, name, namespace) {
     execSync(`kubectl get ${kind} ${name} --namespace=${namespace}`, {
       stdio: "inherit",
     });
+
     return true;
   } catch (err) {
     return false;
@@ -20,25 +21,16 @@ function checkResourceExists(kind, name, namespace) {
 }
 
 function createAuthServiceResources() {
-  if (!checkResourceExists("secret", "auth-service-secrets", namespace)) {
-    var { privateKey, publicKey } = generateKeyPairSync("rsa", {
-      modulusLength: 2048,
-      publicKeyEncoding: {
-        type: "spki",
-        format: "pem",
-      },
-      privateKeyEncoding: {
-        type: "pkcs8",
-        format: "pem",
-      },
-    });
+  const accessTokenSecret = randomBytes(32).toString("base64");
+  const refreshTokenSecret = randomBytes(32).toString("base64");
 
+  if (!checkResourceExists("secret", "auth-service-secrets", namespace)) {
     console.log("creating k8s secret for auth-service-secrets...");
 
     execSync(
       `kubectl create secret generic auth-service-secrets \
-      --from-literal=JWT_PRIVATE_KEY="${privateKey}" \
-      --from-literal=JWT_PUBLIC_KEY="${publicKey}" \
+      --from-literal=ACCESS_TOKEN_SECRET="${accessTokenSecret}" \
+      --from-literal=REFRESH_TOKEN_SECRET="${refreshTokenSecret}" \
       --namespace="${namespace}"`,
       { stdio: "inherit" }
     );
