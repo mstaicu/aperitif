5c4d8d18e2d213139742737d6b740dcd2d955774
Mircea Staicu <md.staicu@yahoo.com>
2025-01-07 14:38:18 +0000

    add secrets-rbac component

--- k8s/infra/overlays/dev/auth/scripts/create-k8s-secret.sh
+++ k8s/infra/overlays/dev/auth/scripts/create-k8s-secret.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
-# Read secrets from shared volume dir
-kubectl create secret generic ${SECRET_NAME} \
+if kubectl get secret $SECRET_NAME -n $NAMESPACE > /dev/null 2>&1; then
+  exit 0
+fi
+
+kubectl create secret generic $SECRET_NAME \
   --from-file=ACCESS_TOKEN_PRIVATE_KEY=/secrets/access_token_private_key.pem \
   --from-file=ACCESS_TOKEN_PUBLIC_KEY=/secrets/access_token_public_key.pem \
-  --from-file=REFRESH_TOKEN_SECRET=secrets/refresh_token_secret.txt \
-  -n ${NAMESPACE} || echo "Secret already exists."
+  --from-file=REFRESH_TOKEN_SECRET=/secrets/refresh_token_secret.txt \
+  -n $NAMESPACE
--- k8s/infra/overlays/dev/auth/scripts/generate-secrets.mjs
+++ k8s/infra/overlays/dev/auth/scripts/generate-secrets.mjs
@@ -2,8 +2,6 @@
 import fs from "node:fs/promises";
 import { generateKeyPairSync, randomBytes } from "node:crypto";
 
-console.log("🔑 Generating RSA key pair...");
-
 var { privateKey, publicKey } = generateKeyPairSync("rsa", {
   modulusLength: 2048,
   publicKeyEncoding: { type: "spki", format: "pem" },
@@ -13,9 +11,5 @@ var { privateKey, publicKey } = generateKeyPairSync("rsa", {
 await fs.writeFile("/secrets/access_token_private_key.pem", privateKey);
 await fs.writeFile("/secrets/access_token_public_key.pem", publicKey);
 
-console.log("🔑 Generating random refresh token secret...");
-
 var refreshTokenSecret = randomBytes(32).toString("base64");
 await fs.writeFile("/secrets/refresh_token_secret.txt", refreshTokenSecret);
-
-console.log("✅ Secrets generated successfully!");
