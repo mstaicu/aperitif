d2e36abd3df6783c8094c41e1e8da5159191a52f
Mircea Staicu <md.staicu@yahoo.com>
2025-06-19 15:51:50 +0100

    move to infra v3

--- k8s/.skaffold/post-deploy.mjs
+++ k8s/.skaffold/post-deploy.mjs
@@ -1,9 +1,9 @@
 import { execSync } from "node:child_process";
-import { mkdtempSync, rmSync } from "node:fs";
+import fs from "node:fs";
 import { tmpdir } from "node:os";
 import path from "node:path";
 
-var DOMAIN = process.env.DOMAIN;
+var DOMAIN = process.env.DOMAIN || "tma.com";
 
 if (!DOMAIN) {
   console.error(
@@ -12,25 +12,26 @@ if (!DOMAIN) {
   process.exit(1);
 }
 
-var tools = ["mkcert"];
+var hostsContent = fs.readFileSync("/etc/hosts", "utf8");
 
-tools.forEach((cmd) => {
-  try {
-    execSync(`command -v ${cmd}`, { stdio: "ignore" });
-    console.log(`✅ ${cmd} is already installed.`);
-  } catch {
-    console.error(`❌ ${cmd} is not installed. Please install ${cmd}.`);
-    process.exit(1);
-  }
-});
+if (!hostsContent.includes(`127.0.0.1 ${DOMAIN}`)) {
+  console.log(`Adding 127.0.0.1 ${DOMAIN} to /etc/hosts`);
+  execSync(`echo "127.0.0.1 ${DOMAIN}" | sudo tee -a /etc/hosts`);
+} else {
+  console.log(`📍 127.0.0.1 ${DOMAIN} already exists in /etc/hosts`);
+}
 
-const TRAEFIK_NAMESPACE = "traefik";
-const TRAEFIK_CERT = "traefik-tls";
+var TRAEFIK_NAMESPACE = "traefik";
+var APP_NAMESPACE = "app";
+var LINKERD_NAMESPACE = "linkerd";
+var TRAEFIK_CERT = "traefik-tls";
 
-const traefikDir = mkdtempSync(path.join(tmpdir(), "traefik-tls-"));
+var traefikDir = fs.mkdtempSync(path.join(tmpdir(), "traefik-tls-"));
 
-const traefikCrt = path.join(traefikDir, "tls.crt");
-const traefikKey = path.join(traefikDir, "tls.key");
+var traefikCrt = path.join(traefikDir, "tls.crt");
+var traefikKey = path.join(traefikDir, "tls.key");
+
+execSync("mkcert -install", { stdio: "inherit" });
 
 execSync(
   `mkcert -cert-file "${traefikCrt}" -key-file "${traefikKey}" ${DOMAIN} "*.${DOMAIN}"`,
@@ -40,5 +41,93 @@ execSync(
 execSync(
   `kubectl -n ${TRAEFIK_NAMESPACE} create secret tls ${TRAEFIK_CERT} --cert="${traefikCrt}" --key="${traefikKey}"`
 );
+execSync(
+  `kubectl -n ${APP_NAMESPACE} create secret tls ${TRAEFIK_CERT} --cert="${traefikCrt}" --key="${traefikKey}"`
+);
 
-rmSync(traefikDir, { recursive: true });
+// import { writeFileSync } from "fs";
+// import { generateKeyPairSync, randomBytes } from "node:crypto";
+
+// const { privateKey, publicKey } = generateKeyPairSync("rsa", {
+//   modulusLength: 2048,
+//   publicKeyEncoding: { type: "spki", format: "pem" },
+//   privateKeyEncoding: { type: "pkcs8", format: "pem" },
+// });
+// const refreshTokenSecret = randomBytes(32).toString("base64");
+
+// writeFileSync("/secrets/access_token_private_key.pem", privateKey);
+// writeFileSync("/secrets/access_token_public_key.pem", publicKey);
+// writeFileSync("/secrets/refresh_token_secret.txt", refreshTokenSecret);
+
+// nsc add operator --name TMA --sys --generate-signing-key
+
+// nsc edit operator --require-signing-keys
+
+// nsc add account TMA
+
+// nsc edit account TMA --sk generate
+
+// # This is how you enable Jetstream for an account
+// # https://www.synadia.com/newsletter/nats-weekly-27
+// nsc edit account TMA \
+//   --js-mem-storage -1 \
+//   --js-disk-storage -1 \
+//   --js-streams -1 \
+//   --js-consumer -1
+
+// nsc add user --account SYS sys
+// nsc add user --account TMA auth
+
+// nsc generate creds --account SYS --name sys > /secrets/sys.creds
+// nsc generate creds --account TMA --name auth > /secrets/auth.creds
+
+// nsc generate config --mem-resolver --sys-account SYS > /secrets/resolver.conf
+
+// kubectl create secret generic nats-resolver \
+//   --from-file=/secrets/resolver.conf \
+//   -n $NAMESPACE \
+//   --dry-run=client -o yaml | kubectl apply -f -
+
+// kubectl create secret generic nats-auth-creds \
+//   --from-file=/secrets/auth.creds \
+//   -n $NAMESPACE \
+//   --dry-run=client -o yaml | kubectl apply -f -
+
+// kubectl create secret generic nats-sys-creds \
+//   --from-file=/secrets/sys.creds \
+//   -n $NAMESPACE \
+//   --dry-run=client -o yaml | kubectl apply -f -
+
+var linkerdDir = fs.mkdtempSync(path.join(tmpdir(), "linkerd-certs-"));
+
+process.chdir(linkerdDir);
+
+execSync(
+  `step certificate create root.linkerd.cluster.local ca.crt ca.key \
+  --profile root-ca --no-password --insecure --not-after=87600h`,
+  { stdio: "inherit" }
+);
+
+execSync(
+  `step certificate create identity.linkerd.cluster.local issuer.crt issuer.key \
+  --profile intermediate-ca --not-after 8760h --no-password --insecure \
+  --ca ca.crt --ca-key ca.key`,
+  { stdio: "inherit" }
+);
+
+execSync(
+  `kubectl -n ${LINKERD_NAMESPACE} create secret generic linkerd-identity-issuer \
+    --from-file=tls.crt=${linkerdDir}/issuer.crt \
+    --from-file=tls.key=${linkerdDir}/issuer.key \
+    --from-file=ca.crt=${linkerdDir}/ca.crt`,
+  { stdio: "inherit" }
+);
+
+execSync(
+  `kubectl -n ${LINKERD_NAMESPACE} create configmap linkerd-identity-trust-roots \
+    --from-file=ca-bundle.crt=${linkerdDir}/ca.crt`,
+  { stdio: "inherit" }
+);
+
+fs.rmSync(traefikDir, { recursive: true });
+fs.rmSync(linkerdDir, { recursive: true });
--- k8s/auth/src/routes/index.mjs
+++ k8s/auth/src/routes/index.mjs
@@ -8,6 +8,9 @@ import { healthRouter } from "./health.mjs";
 const router = Router();
 
 router.use("/", healthRouter);
+router.use("/api/auth/hello", (_, res) => {
+  res.json({ hello: "world" });
+});
 // router.use("/", jwksRouter);
 
 // router.use(serviceAvailability);
