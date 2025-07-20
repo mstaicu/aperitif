47a853ec837d2f24b3e898932ff456a0bf7d8c48
Mircea Staicu <md.staicu@yahoo.com>
2025-07-20 20:26:29 +0100

    add cert and trust manager

---
+++ k8s/scripts/localhost.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+DOMAIN="${DOMAIN:-tma.com}"
+TRAEFIK_NAMESPACE="${TRAEFIK_NAMESPACE:-traefik}"
+TRAEFIK_TLS_SECRET="${TRAEFIK_TLS_SECRET:-traefik-tls}"
+
+TMPDIR=$(mktemp -d -t traefik-certs)
+trap "rm -rf $TMPDIR" EXIT
+
+CERT_FILE="$TMPDIR/tls.crt.pem"
+KEY_FILE="$TMPDIR/tls.key.pem"
+
+grep -q "127.0.0.1 $DOMAIN" /etc/hosts || echo "127.0.0.1 $DOMAIN" | sudo tee -a /etc/hosts
+
+mkcert -install
+mkcert -cert-file "$CERT_FILE" -key-file "$KEY_FILE" "*.$DOMAIN" "$DOMAIN" localhost 127.0.0.1 ::1
+
+kubectl create secret tls "$TRAEFIK_TLS_SECRET" \
+  --cert="$CERT_FILE" \
+  --key="$KEY_FILE" \
+  --namespace "$TRAEFIK_NAMESPACE" \
+  --dry-run=client \
+  --output yaml | kubectl apply -f -
+
+# kubectl -n "$LINKERD_NAMESPACE" create secret generic linkerd-identity-issuer \
+#   --from-file=tls.crt=intermediate_ca.crt \
+#   --from-file=tls.key=intermediate_ca_key.pem \
+#   --from-file=ca.crt=root_ca.crt \
+#   --dry-run=client -o yaml | kubectl apply -f -
+
+# # --- ConfigMap for Linkerd trust roots ---
+# kubectl -n "$LINKERD_NAMESPACE" create configmap linkerd-identity-trust-roots \
+#   --from-file=ca-bundle.crt=root_ca.crt \
+#   --dry-run=client -o yaml | kubectl apply -f -
