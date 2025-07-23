89697789153fb65e0a1a3940c49cb456173a85db
Mircea Staicu <md.staicu@yahoo.com>
2025-07-23 22:54:18 +0100

    rename auth to identity service

--- k8s/identity/src/server.mjs
+++ k8s/identity/src/server.mjs
@@ -15,7 +15,7 @@ var server = addGracefulShutdown(
 var connection = await createConnection(nconf.get("MONGO_DB_URI"), {
   autoIndex: false,
   bufferCommands: false,
-  dbName: "auth",
+  dbName: "identitys",
 });
 
 // var nc = await connect();
