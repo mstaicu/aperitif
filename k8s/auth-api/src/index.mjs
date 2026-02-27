ca7522918016bd868c6270a0d454094063fdd06b
Mircea Staicu <md.staicu@yahoo.com>
2026-02-27 14:23:46 +0000

    conf nats

--- k8s/auth-api/src/index.mjs
+++ k8s/auth-api/src/index.mjs
@@ -3,9 +3,14 @@ import nconf from "nconf";
 nconf
   .env()
   .required([
+    "DATABASE_URL",
     "JWT_PRIVATE_KEY_PATH",
     "JWT_PUBLIC_KEY_PATH",
-    "DATABASE_URL",
+
+    "NATS_URL",
+    "NATS_USER",
+    "NATS_PASSWORD",
+
     "ORIGIN",
   ]);
 
--- k8s/auth-api/src/plugins/nats.mjs
+++ k8s/auth-api/src/plugins/nats.mjs
@@ -1,20 +1,27 @@
 import { jetstream, jetstreamManager } from "@nats-io/jetstream";
 import { connect } from "@nats-io/transport-node";
 import fp from "fastify-plugin";
+import nconf from "nconf";
 
 export const natsPlugin = fp(async (fastify) => {
-  const servers = Array.from(Array(3)).map(
-    (_, index) =>
-      `nats://nats-depl-${index}.nats-headless.nats.svc.cluster.local:4222`,
-  );
-
   const nc = await connect({
     name: "auth-api",
-    servers,
+    servers: [nconf.get("NATS_URL")],
+    user: nconf.get("NATS_USER"),
+    pass: nconf.get("NATS_PASSWORD"),
   });
 
   const js = jetstream(nc);
   const jsm = await jetstreamManager(nc);
+
+  try {
+    await jsm.streams.add({
+      name: "auth",
+      subjects: ["auth.>"],
+      storage: "file",
+      num_replicas: 3,
+    });
+  } catch {}
 
   fastify.decorate("nc", nc);
   fastify.decorate("js", js);
