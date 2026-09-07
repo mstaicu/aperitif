7c2a0ac97fba54b9aacbdc82465ea1cdeb37d1dc
Mircea Staicu <md.staicu@yahoo.com>
2026-09-07 11:26:57 +0100

    update workflows

--- k8s/domains/plans/workloads/accounts-projection/src/server.mjs
+++ k8s/domains/plans/workloads/accounts-projection/src/server.mjs
@@ -29,7 +29,6 @@ const info = await jsm.consumers.add("ACCOUNTS", {
 const consumer = js.consumers.getConsumerFromInfo(info);
 const messages = await consumer.consume({
   abort_on_missing_resource: true,
-  max_messages: 1, // Process one message at a time.
 });
 
 await using probeServer = createServer(async (req, res) => {
