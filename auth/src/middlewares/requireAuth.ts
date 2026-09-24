12c847fd0e7bf9a55f3981327ed64ba11289a7cd
Mircea Staicu <md.staicu@yahoo.com>
2026-09-24 18:50:56 +0100

    restructure platform at repository root

--- auth/src/middlewares/requireAuth.ts
+++
@@ -1,8 +0,0 @@
-import type { Request, Response, NextFunction } from 'express';
-
-import { RequestAuthenticationError } from '../errors';
-
-const requireAuthentication = (req: Request, _: Response, next: NextFunction) =>
-  req.user ? next() : next(new RequestAuthenticationError());
-
-export { requireAuthentication };
--- auth/src/services/validation.ts
+++
@@ -1,9 +0,0 @@
-import Ajv from 'ajv';
-import addErrors from 'ajv-errors';
-import addFormats from 'ajv-formats';
-
-const ajv = new Ajv({ allErrors: true });
-
-addErrors(addFormats(ajv));
-
-export { ajv };
