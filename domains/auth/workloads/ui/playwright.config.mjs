bfbfd8cbaeae45ca9bfb45b6f37ea1973fba952e
Mircea Staicu <md.staicu@yahoo.com>
2026-09-24 19:38:02 +0100

    allow auth e2e against deployed environments

--- domains/auth/workloads/ui/playwright.config.mjs
+++ domains/auth/workloads/ui/playwright.config.mjs
@@ -4,7 +4,7 @@ export default defineConfig({
   reporter: "list",
   testDir: "test/e2e",
   use: {
-    baseURL: "http://localhost",
+    baseURL: process.env.PLAYWRIGHT_BASE_URL ?? "http://localhost",
     trace: "retain-on-failure",
   },
 });
